870dd1a7cea84daa139de0d09821d69252c576c1
lsrom <lukas.srom@gmail.com>
2018-03-27 18:29:59 +0200

    Add interfaces

---
+++ app/src/main/java/cz/lsrom/webviewtest/FragmentLifecycle.java
@@ -0,0 +1,10 @@
+package cz.lsrom.webviewtest;
+
+/**
+ * @author Lukas Srom <lukas.srom@gmail.com>
+ */
+public interface FragmentLifecycle {
+    void onResumeFragment();
+
+    void reloadWebView();
+}
---
+++ app/src/main/java/cz/lsrom/webviewtest/ITabChanger.java
@@ -0,0 +1,8 @@
+package cz.lsrom.webviewtest;
+
+/**
+ * @author Lukas Srom <lukas.srom@gmail.com>
+ */
+public interface ITabChanger {
+    void showWebViewTab ();
+}
