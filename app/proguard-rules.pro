33a5c0f1f12ccdc93bbbaea61639f9cf55f528a4
lsrom <lukas.srom@gmail.com>
2020-02-20 20:47:27 +0100

    FIxing up styles

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,6 +1,5 @@
 -dontwarn android.support.**
 
--ignorewarning
 -dontoptimize
 -optimizations !code/simplification/arithmetic
 -keepattributes Exceptions,InnerClasses,Signature,Deprecated,SourceFile,LineNumberTable,*Annotation*,EnclosingMethod
--- app/src/main/kotlin/cz/lsrom/webviewtest/app/App.kt
+++ app/src/main/kotlin/cz/lsrom/webviewtest/app/App.kt
@@ -5,13 +5,17 @@
 package cz.lsrom.webviewtest.app
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import cz.lsrom.webviewtest.BuildConfig
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 internal class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Fabric.with(applicationContext, Crashlytics())
 
         if (BuildConfig.DEBUG){
             Timber.plant(Timber.DebugTree())
