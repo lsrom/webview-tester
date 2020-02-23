24ef430c058447245f08559100ca44bfd90c6511
lsrom <lukas.srom@gmail.com>
2020-02-23 14:13:52 +0100

    Remove unused code

--- app/src/main/kotlin/cz/lsrom/webviewtest/logs/ui/LogView.kt
+++ app/src/main/kotlin/cz/lsrom/webviewtest/logs/ui/LogView.kt
@@ -4,7 +4,6 @@
 
 package cz.lsrom.webviewtest.logs.ui
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.*
@@ -14,7 +13,6 @@ import cz.lsrom.webviewtest.logs.platform.LogPresenter
 import cz.lsrom.webviewtest.tabs.ui.BaseTabView
 import kotlinx.android.synthetic.main.log_view.*
 import timber.log.Timber
-
 
 internal class LogView : BaseTabView() {
 
@@ -69,7 +67,6 @@ internal class LogView : BaseTabView() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.log_view_menu_share -> presenter.shareLogs(requireContext())
-            R.id.log_view_menu_search -> Timber.d("CLick search")
         }
         return super.onOptionsItemSelected(item)
     }
